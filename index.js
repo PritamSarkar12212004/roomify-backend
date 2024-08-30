e2f5090443debb2afb2ddfe6d08bcd5a4f3d6ba2
pritam sarkar <pritam7796419792@gmail.com>
2024-08-31 00:23:20 +0530

    aa

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://roomify-alpha.vercel.app/",
+    origin: "https://roomify-alpha.vercel.app",
   })
 );
 
